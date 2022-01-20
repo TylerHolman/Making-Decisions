29a8d96d433ffe980b0e4fcbab5b354c99f35a6c
Tyler Holman <holman.tyken@yahoo.com>
2022-01-19 18:47:55 -0600

    Initial commit

--- lab.js
+++ lab.js
@@ -7,7 +7,13 @@
 */
 
 //CODE HERE
+let = lovesCode = true
 
+if(lovesCode === false){
+  console.log(`I love to code!`)
+}else{
+  console.log(`Coding has its challenges`)
+}
 // For problems 2-3 use the following lines of code:
 var amysAge = 29
 var brittanisAge = 34
@@ -21,7 +27,13 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
-
+if(amysAge > brittanisAge){
+  console.log(`Amy is older`)
+} else if (amysAge === brittanisAge){
+  console.log(`same age`)
+} else {
+  console.log(`Brittani is older`)
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -30,12 +42,16 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
-
+if(amysBirthYear === brittanisBirthYear){
+  console.log(`Amy & Brittani were born in the same year.`)
+} else {
+  console.log(`Amy & Brittani were not born in the same year.`)
+}
 
 ////////// PROBLEM 4 //////////
 
-let temperature = 55
-let rain = true
+let temperature = 66
+let rain = false
 
 /*
   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
@@ -43,7 +59,19 @@ let rain = true
 */
 
 //CODE HERE
-
+if(temperature >= 80 && rain === true){
+  console.log(`Wear a T-shirt and take an umbrella`)
+} else if(temperature <80 && temperature >60 && rain === true){
+  console.log(`Wear a rain jacket`)
+ } else if(temperature <= 60 && rain === true){ 
+   console.log(`Wear a jacket and carry an umbrella`)
+} else if(temperature < 32 && rain !== true){
+  console.log(`Wear a heavy jacket`)
+} else if(temperature >32 && temperature < 60 &&  rain !== true){
+  console.log(`Wear a t-shirt and jacket`)
+}else if(temperature >60 && rain !== true){
+  console.log(`Wear a t-shirt`)
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -52,7 +80,8 @@ let rain = true
 */
 
 //CODE HERE
-
+for(let i = 0; i < 11; i++)
+  console.log(`hello`)
 
 ////////// PROBLEM 6 //////////
 
@@ -61,7 +90,8 @@ let rain = true
 */
 
 //CODE HERE
-
+for(let i = 1; i <11; i++)
+console.log(i)
 
 ////////// PROBLEM 7 //////////
 
@@ -70,18 +100,24 @@ let rain = true
 */
 
 //CODE HERE
+for(let i = 10; i >= 0; i--){
+console.log(i)
+}
 
-let score = 0
-let passingScore = 7
+
 
 ////////// PROBLEM 8 //////////
-
+let score = 0
+let passingScore = 7
 /*
   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
 */
 
 //CODE HERE
-
+while(score < passingScore){
+console.log(`Your score is not high enough`)
+score += 1
+}
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
@@ -92,11 +128,26 @@ let passingScore = 7
 */
 
 //CODE HERE
+let changeMyMind = true
+
+if(changeMyMind === true){
+  changeMyMind = false;
+  console.log(changeMyMind);
+} else {
+  changeMyMind = true
+  console.log(changeMyMind)
+}
+
 
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
+
+changeMyMind = !changeMyMind
+console.log(changeMyMind)
+
+
 
 
 
@@ -108,3 +159,54 @@ let z = 5
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+// while(z > 0){
+//   for(z; z > 0; z-=1)
+//   console.log(z)
+// }
+// let w = 4
+// while(w >= 0){
+//   for(w; w > 0; w-=1)
+//   console.log(w)
+// }
+// let r = 3
+// while(r > 0){
+//   for(r; r > 0; r-=1)
+//   console.log(r)
+// }
+
+while(z > 0){
+  for(z; z > 0; z -= 1)
+
+  console.log(z)
+
+}
+let q = 4
+
+while(q > 0){
+  for(q; q > 0; q -=1)
+
+  console.log(q)
+}
+
+let w = 3
+
+while(w > 0){
+  for(w; w > 0; w -=1)
+
+  console.log(w)
+}
+
+let r = 2
+
+while(r > 0){
+  for(r; r > 0; r -=1)
+
+  console.log(r)
+}
+let k = 1
+
+while(k > 0){
+  for(k; k > 0; k -=1)
+
+  console.log(k)
+}
